d552e53a37284c0f9779c1d24fbcd64952a933a3
kaziiqbal880 <kaziiqbal880@gmail.com>
2026-08-25 07:11:03 -0700

    added profile update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
+"use client"
+import { authClient } from '@/lib/auth-client';
+import { Button } from '@heroui/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 
 const Navbar = () => {
+
+    const { data: sessions, isPending } = authClient.useSession()
+    console.log(sessions)
+    const user = sessions?.user;
+    console.log(user)
+
     return (
         <div className="border-b px-2">
             <nav className=" flex justify-between items-center py-3 max-w-7xl mx-auto w-full">
@@ -34,16 +43,32 @@ const Navbar = () => {
                     </li>
                 </ul>
 
-                <div className="flex gap-4">
-                    <ul className="flex items-center gap-3  text-sm">
-                        <li>
-                            <Link href={"/signup"}>SignUp</Link>
-                        </li>
-                        <li>
-                            <Link href={"/signin"}>SignIn</Link>
-                        </li>
-                    </ul>
-                </div>
+                {user ? <div className='flex gap-2 justify-center items-center'>
+                    <div>
+                        <h1>{`Hello! ${user.name} `}</h1>
+                        <Link href={'/'}>
+                            <Button onClick={() => authClient.signOut()} variant='outline'>Log Out</Button>
+                        </Link>
+                    </div>
+                    <Image
+                        src='/hero.jpg'
+                        alt={user.name}
+                        width={40}
+                        height={50}
+                        className="rounded-full"
+                    />
+                </div> :
+                    <div className="flex gap-4">
+                        <ul className="flex items-center gap-3  text-sm">
+                            <li>
+                                <Link href={"/signup"}>SignUp</Link>
+                            </li>
+                            <li>
+                                <Link href={"/signin"}>SignIn</Link>
+                            </li>
+                        </ul>
+                    </div>}
+
             </nav>
         </div>
     );
